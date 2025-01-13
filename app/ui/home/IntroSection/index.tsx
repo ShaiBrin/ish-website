37e6e94add5e23058e2622fe4058a00ffe7732cc
ShaiBrin <Ishr_Shai@live.ca>
2025-01-13 18:57:53 -0500

    refac: Removed the shadow on the card to make it more user friendly

--- app/ui/home/IntroSection/index.tsx
+++ app/ui/home/IntroSection/index.tsx
@@ -10,7 +10,7 @@ const IntroSide : React.FC<ProfileProps> = ({ profile }) => {
     return (
         <div className="flex flex-grow">
             <div className="w-full">
-                    <div className="mb-8 bg-white p-8 rounded-xl shadow-lg w-full max-w-4xl">
+                    <div className="mb-8 bg-white p-8 rounded-xl w-full max-w-4xl">
                         <h1 className="text-4xl font-bold text-center mb-6">{profile.name}</h1>
                             <p className="text-lg text-center">
                                 {profile.description}
