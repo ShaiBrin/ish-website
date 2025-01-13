56be1ea8fc606cc15e8c44d52dcdaee3f0f46a8b
ShaiBrin <Ishr_Shai@live.ca>
2025-01-13 12:27:09 -0500

    refac: Removed the huge spacing below the profile and intro section

--- app/ui/home/techStack/index.tsx
+++ app/ui/home/techStack/index.tsx
@@ -17,29 +17,30 @@ const ResponsiveStack: React.FC<ProgInterface> = ({ prog, text }) => {
         <Typography 
           variant="h4" 
           gutterBottom
-          className="font-semibold text-2xl text-gray-800 italic mb-6"
+          className="font-semibold text-2xl text-gray-800 italic mb-6 text-center md:text-left"
         >
           {text}
       </Typography>
         <div>
-          <Stack
-          direction={{ xs: 'row', sm: 'row', md: 'row' }} 
-          spacing={{ xs: 2, sm: 2, md: 4 }} 
-        >
-              {prog.map((tech, index) => (
-              <div key={index} className="flex flex-col items-center group">
-                <Image
-                  className="w-10 h-10 object-contain transition-transform duration-300 transform group-hover:scale-110"
-                  src={tech.image}
-                  alt={`${tech.name} logo`}
-                  width={700}
-                  height={700}
-                  style={{ borderRadius: '50%' }}
-                />
-              <span className="mt-2 text-sm">{tech.name}</span>
-            </div>
-            ))}
-          </Stack>
+        <Stack
+  direction={{ xs: 'row', sm: 'row', md: 'row' }}
+  spacing={{ xs: 2, sm: 2, md: 4 }}
+  justifyContent={{ xs: 'center', sm: 'start', md: 'start' }} // Centers on small screens
+>
+  {prog.map((tech, index) => (
+    <div key={index} className="flex flex-col items-center group">
+      <Image
+        className="w-10 h-10 object-contain transition-transform duration-300 transform group-hover:scale-110"
+        src={tech.image}
+        alt={`${tech.name} logo`}
+        width={700}
+        height={700}
+        style={{ borderRadius: '50%' }}
+      />
+      <span className="mt-2 text-sm">{tech.name}</span>
+    </div>
+  ))}
+</Stack>
         </div>
       </div>
     );
